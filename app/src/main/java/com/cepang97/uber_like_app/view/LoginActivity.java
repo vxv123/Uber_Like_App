4777e0aaa6ba5caba8f5368525629a33abf865f6
Ce Pang <pcdota123@gmail.com>
2022-10-06 16:57:50 -0700

    New Activity: Login

---
+++ app/src/main/java/com/cepang97/uber_like_app/view/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.cepang97.uber_like_app.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.cepang97.uber_like_app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
