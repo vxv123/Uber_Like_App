5da03f02d89714ad12d28a7c098797b316bf2144
Ce Pang <pcdota123@gmail.com>
2022-10-06 16:33:39 -0700

    Create a new App

---
+++ app/src/main/java/com/cepang97/uber_like_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cepang97.uber_like_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
