8e95632797be02c94277cce4906a6a99298421d9
Ce Pang <pcdota123@gmail.com>
2022-10-06 21:23:46 -0700

    Connect firebase with current project

---
+++ app/src/main/java/com/cepang97/uber_like_app/controller/LoginHandler.java
@@ -0,0 +1,14 @@
+package com.cepang97.uber_like_app.controller;
+
+public class LoginHandler {
+
+    /**
+     * Check whether the info user insert is an existed employee info in firebase
+     * @param email_address as string
+     * @param password as string
+     * @return false if the employee is not existed, return true if it is existed.
+     */
+    public static boolean employee_credential_checking(String email_address, String password){
+        return true;
+    }
+}
