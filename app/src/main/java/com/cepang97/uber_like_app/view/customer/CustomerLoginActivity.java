a6862cae9501499776ca83cbed132ba090862beb
Ce Pang <pcdota123@gmail.com>
2022-10-15 15:50:45 -0700

    Customer/Employee Login Activities

--- app/src/main/java/com/cepang97/uber_like_app/view/customer/CustomerLoginActivity.java
+++ app/src/main/java/com/cepang97/uber_like_app/view/customer/CustomerLoginActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,7 +17,6 @@ import com.cepang97.uber_like_app.R;
 public class CustomerLoginActivity extends AppCompatActivity {
     EditText username;
     EditText password;
-    CheckBox employee, customer;
     Button submit;
 
     @Override
@@ -27,11 +25,7 @@ public class CustomerLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customer_login);
         username = findViewById(R.id.username_in_login);
         password = findViewById(R.id.password_in_login);
-        employee = findViewById(R.id.employee_checkbox_in_login);
-        customer = findViewById(R.id.customer_checkbox_in_login);
         submit = findViewById(R.id.btn_login);
-
-        only_one_checkbox_checked(employee, customer);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,19 +48,6 @@ public class CustomerLoginActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
-        if(!employee.isChecked() && !customer.isChecked()){
-            Toast.makeText(CustomerLoginActivity.this, "Are you an employee or a customer?", Toast.LENGTH_SHORT).show();
-        }
-        else if(employee.isChecked()){
-
-        }
-        else if(customer.isChecked()){
-
-        }
     }
 
     public boolean input_content_checking(EditText username, EditText password){
@@ -79,29 +60,5 @@ public class CustomerLoginActivity extends AppCompatActivity {
             return false;
         }
         return true;
-    }
-
-
-    /**
-     * Set Two Checkboxes that can only be checked one each time.
-     * @param customer customer as CheckBox
-     * @param employee employee as CheckBox
-     */
-    public void only_one_checkbox_checked(CheckBox customer, CheckBox employee){
-        employee.setOnCheckedChangeListener(
-                (buttonView, isChecked) -> {
-                    if(isChecked){
-                        customer.setChecked(false);
-                    }
-                }
-        );
-
-        customer.setOnCheckedChangeListener(
-                (buttonView, isChecked) -> {
-                    if(isChecked){
-                        employee.setChecked(false);
-                    }
-                }
-        );
     }
 }
--- app/src/main/java/com/cepang97/uber_like_app/view/employee/EmployeeLoginActivity.java
+++ app/src/main/java/com/cepang97/uber_like_app/view/employee/EmployeeLoginActivity.java
@@ -3,14 +3,66 @@ package com.cepang97.uber_like_app.view.employee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.amplifyframework.auth.AuthException;
+import com.amplifyframework.auth.result.AuthSignInResult;
+import com.amplifyframework.core.Amplify;
 import com.cepang97.uber_like_app.R;
+import com.cepang97.uber_like_app.view.customer.CustomerLoginActivity;
 
 public class EmployeeLoginActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_login);
+        username = findViewById(R.id.username_in_employee_login);
+        password = findViewById(R.id.password_in_employee_login);
+        submit = findViewById(R.id.btn_in_employee_login);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(input_content_checking(username, password)){
+                    Amplify.Auth.signIn(
+                            username.getText().toString(),
+                            password.getText().toString(),
+                            this::onLoginSuccess,
+                            this::onLoginError
+
+                    );
+                }
+
+
+            }
+
+            private void onLoginError(AuthException e) {
+
+            }
+
+            private void onLoginSuccess(AuthSignInResult authSignInResult) {
+
+            }
+        });
+    }
+
+    public boolean input_content_checking(EditText username, EditText password){
+        if(TextUtils.isEmpty(username.getText())){
+            Toast.makeText(EmployeeLoginActivity.this, "Username is empty!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(TextUtils.isEmpty(password.getText())){
+            Toast.makeText(EmployeeLoginActivity.this, "Password is empty!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
