b6b4a65ea1fd21f86201a9ccd09e7cdcd6b2bffc
Ce Pang <pcdota123@gmail.com>
2023-01-12 22:35:40 -0500

    Customer Basic Map Model Done

--- app/src/main/java/com/cepang97/uber_like_app/view/customer/CustomerMainActivity.java
+++ app/src/main/java/com/cepang97/uber_like_app/view/customer/CustomerMainActivity.java
@@ -2,9 +2,12 @@ package com.cepang97.uber_like_app.view.customer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 
 import com.cepang97.uber_like_app.R;
+
 
 public class CustomerMainActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class CustomerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_main);
+
+        Fragment fragment = new CustomerMapFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout_in_customer, fragment)
+                .commit();
+
     }
 }
---
+++ app/src/main/java/com/cepang97/uber_like_app/view/customer/CustomerMapFragment.java
@@ -0,0 +1,36 @@
+package com.cepang97.uber_like_app.view.customer;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cepang97.uber_like_app.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class CustomerMapFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_customer_map, container, false);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.customer_map);
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                // When map is loaded.
+
+            }
+        });
+        return view;
+    }
+}
