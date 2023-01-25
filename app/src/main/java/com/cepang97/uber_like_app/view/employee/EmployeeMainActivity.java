12d67943e22c294f1de206ec09d6fda5b1093e9f
Ce Pang <pcdota123@gmail.com>
2023-01-24 19:52:13 -0500

    Basic Employee Map Done

--- app/src/main/java/com/cepang97/uber_like_app/view/employee/EmployeeMainActivity.java
+++ app/src/main/java/com/cepang97/uber_like_app/view/employee/EmployeeMainActivity.java
@@ -1,10 +1,12 @@
 package com.cepang97.uber_like_app.view.employee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 import com.cepang97.uber_like_app.R;
+import com.cepang97.uber_like_app.view.customer.CustomerMapFragment;
 
 public class EmployeeMainActivity extends AppCompatActivity {
 
@@ -12,5 +14,12 @@ public class EmployeeMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_main);
+
+        Fragment fragment = new CustomerMapFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout_in_employee, fragment)
+                .commit();
     }
 }
